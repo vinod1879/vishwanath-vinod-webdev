e4e93cd8e0052228dcca482d62bba2a11fa7a8d9
Vinod Vishwanath <gmvinodv@gmail.com>
2017-05-12 21:51:47 -0400

    Integrated with MLab

--- test/app.js
+++ test/app.js
@@ -5,11 +5,11 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
     var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
+    if(process.env['MLAB_USERNAME']) { // check if running remotely
+        var username = process.env['MLAB_USERNAME']; // get from environment
+        var password = process.env['MLAB_PASSWORD'];
         connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+        connectionString += '@ds137271.mlab.com:37271/heroku_95pqxbsw';
     }
 
     var mongoose = require("mongoose");
